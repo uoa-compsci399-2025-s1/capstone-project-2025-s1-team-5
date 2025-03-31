246b26ddd4ab9cc5d1d7e605868cc542fcc58f69
Gavinzld <gavinzlding@gmail.com>
2025-03-31 14:57:39 +1300

    Removed node_modules from Git tracking and added .gitignore. Added scripts for startup and server communication

--- cms/backend/server.js
+++ cms/backend/server.js
@@ -4,3 +4,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 const app = express();
 app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
