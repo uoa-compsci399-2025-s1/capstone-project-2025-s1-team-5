5a12e29b1690390f258de5a232e0a448c0102eae
Derrick Trang <derrick.trang100@hotmail.com>
2025-04-03 21:52:44 +1300

    Added schemas for MongoDB

---
+++ cms/backend/models/Comment.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const CommentSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  body: { type: String, required: true },
+  author: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  published: { type: Boolean, default: false },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("Comment", CommentSchema);
