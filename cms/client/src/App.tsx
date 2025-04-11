7a406ddf5ed0fe17ea5cf06fc537cd01961e6abc
Gavinzld <gavinzlding@gmail.com>
2025-04-12 02:27:26 +1200

    Basic app established

--- cms/client/src/App.tsx
+++ cms/client/src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <Home />
     </div>
   );
 }
