2df153672600c4e1b0d0dd1174c944cc36d1f6ec
JackZhen1 <140671300+JackZhen1@users.noreply.github.com>
2025-04-11 13:49:48 +1200

    testing darkmode on main page

--- MyApp/app/(auth)/home/index.tsx
+++ MyApp/app/(auth)/home/index.tsx
@@ -1,8 +1,11 @@
 import { View, Text } from 'react-native';
+import { useContext } from 'react';
+import { ThemeContext } from '../../contexts/ThemeContext';
 
 export default function HomeScreen() {
+  const { theme, setCustomTheme } = useContext(ThemeContext);
   return (
-    <View>
+    <View style={[{backgroundColor:theme.background}]}>
       <Text>Work in progress...</Text>
     </View>
   );
