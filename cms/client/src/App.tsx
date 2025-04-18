0e3745aa9e1e9ad0dc44942227d7f1f3408c6327
charoncs <lhua218@aucklanduni.ac.nz>
2025-04-18 15:08:44 +1200

    Working Personal Front/Back
    O

--- cms/client/src/App.tsx
+++ cms/client/src/App.tsx
@@ -1,21 +1,39 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
 import Home from './pages/Home';
-import ContentPage from './pages/Content';
-import UsersPage from './pages/Users';
-import AccountPage from './pages/Account';
 
-function App() {
+
+// function App() {
+//   return (
+//     <Router>
+//       <Layout>
+//         <Routes>
+//           <Route path="/" element={<Home />} />
+//           <Route path="/home" element={<Home />} />
+//           <Route path="/modules/content" element={<ContentPage />} />
+//           <Route path="/modules/users" element={<UsersPage />} />
+//           <Route path="/modules/account" element={<AccountPage />} />
+//         </Routes>
+//       </Layout>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+// src/App.tsx
+export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/modules/content" element={<ContentPage />} />
-        <Route path="/modules/users" element={<UsersPage />} />
-        <Route path="/modules/account" element={<AccountPage />} />
-      </Routes>
-    </Router>
+    <div style={{
+      backgroundColor: 'red', 
+      height: '100vh',
+      width: '100vw',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
+      <h1 style={{ color: 'white' }}>Hello World</h1>
+    </div>
   );
 }
-
-export default App;
