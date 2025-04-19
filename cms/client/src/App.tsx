53f8a92a24ebc216b6227dd55e125c938c9453f3
charoncs <lhua218@aucklanduni.ac.nz>
2025-04-19 20:36:22 +1200

    Server can start, database connected.
    Next step: POST/GET data connection from CMS. Fix working overlay

--- cms/client/src/App.tsx
+++ cms/client/src/App.tsx
@@ -1,8 +1,36 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
 import Home from './pages/Home';
+import ContentPage from './pages/Content';
+import UsersPage from './pages/Users';
+import AccountPage from './pages/Account';
+import Layout from './components/Layout';
 
+function App() {
+  return (
+    <Layout>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/modules/content" element={<ContentPage />} />
+        <Route path="/modules/users" element={<UsersPage />} />
+        <Route path="/modules/account" element={<AccountPage />} />
+      </Routes>
+    </Router>
+    </Layout>
+  );
+}
+
+export default App;
+
+
+// import React from 'react';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Layout from './components/Layout';
+// import Home from './pages/Home';
+// import AccountPage from './pages/Account';
+// import ContentPage from './pages/Content';
+// import UsersPage from './pages/Users';
 
 // function App() {
 //   return (
@@ -23,17 +51,19 @@ import Home from './pages/Home';
 // export default App;
 
 // src/App.tsx
-export default function App() {
-  return (
-    <div style={{
-      backgroundColor: 'red', 
-      height: '100vh',
-      width: '100vw',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center'
-    }}>
-      <h1 style={{ color: 'white' }}>Hello World</h1>
-    </div>
-  );
-}
+// export default function App() {
+//   return (
+//     <Layout>
+//       <div style={{
+//         backgroundColor: 'red', 
+//         height: '100vh',
+//         width: '100vw',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center'
+//       }}>
+//         <h1 style={{ color: 'white' }}>Hello World</h1>
+//       </div>
+//     </Layout>
+//   );
+// }
