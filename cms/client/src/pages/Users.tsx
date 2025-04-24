e3de50ed6d539b917df3d2d415bb579063f81f5a
charoncs <lhua218@aucklanduni.ac.nz>
2025-04-24 15:36:22 +1200

    front-end back-end established. Connection is working!!!!!!!!!!!
    Users now able to be manually added via CMS.

--- cms/client/src/pages/Users.tsx
+++ cms/client/src/pages/Users.tsx
@@ -1,16 +1,13 @@
-import React from 'react';
-// import Layout from '../components/Layout';
+import React from "react";
+import CreateUser from "../pages/CreateUser"
 
+const UsersPage = () => {
+  return (
+    <div>
+      <h1>Manage Users</h1>
+      <CreateUser />
+    </div>
+  );
+};
 
-function UsersPage() {
-    return (
-        // <Layout>
-            <div style={{ padding: '1rem' }}>
-            <h1>User Management</h1>
-            <p>Manage Users</p>
-            </div>
-        // </Layout>
-    );
-  }
-  
 export default UsersPage;
