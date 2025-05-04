ffd379523f2a90c4b603384440836e13e9921566
charoncs <lhua218@aucklanduni.ac.nz>
2025-05-04 21:01:06 +1200

    local api fix

--- cms/server/src/middleware/__generated__/routes.ts
+++ cms/server/src/middleware/__generated__/routes.ts
@@ -21,7 +21,7 @@ const expressAuthenticationRecasted = expressAuthentication as (req: ExRequest, 
 const models: TsoaRoute.Models = {
     "RoleType": {
         "dataType": "refEnum",
-        "enums": ["user","admin"],
+        "enums": ["admin","user"],
     },
     // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
     "IUser": {
