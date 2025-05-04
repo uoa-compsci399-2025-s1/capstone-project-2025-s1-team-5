0e527645d9442d18fb148cb8aa5023a88d4449cd
charoncs <lhua218@aucklanduni.ac.nz>
2025-05-04 20:02:57 +1200

    fix

--- app/app/lib/api.ts
+++ app/app/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import * as SecureStore from 'expo-secure-store';
 
-const api = axios.create({ baseURL: 'http://172.23.47.87:3000' });
+const api = axios.create({ baseURL: 'http://localhost:3000' });
 
 api.interceptors.request.use(async (config) => {
   const token = await SecureStore.getItemAsync('USER_TOKEN');
