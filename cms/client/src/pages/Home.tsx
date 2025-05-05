2b7daa2a9bc602716dfab76cec7d30b0423983fb
charoncs <lhua218@aucklanduni.ac.nz>
2025-05-05 15:30:20 +1200

    Added tailwindcss styling

--- cms/client/src/pages/Home.tsx
+++ cms/client/src/pages/Home.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
-// import Layout from '../components/Layout';
 
 function Home() {
   return (
-      // <Layout>
           <div style={{ padding: '1rem',}}>
           <h1>Home Page</h1>
-          <p>Home Page.</p>
+          <p>Welcome to UOA: Your Way</p>
           </div>
-      // </Layout>
+
   );  
 }
 
