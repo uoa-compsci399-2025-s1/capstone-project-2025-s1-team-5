62b600704a56186ceab50a5add89f048bdd16bbf
JackZhen1 <140671300+JackZhen1@users.noreply.github.com>
2025-05-17 22:32:20 +1200

    ngrok and updated project to SDK 53 so its compatible with newest version of expo go

--- app/app/lib/api.ts
+++ app/app/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import * as SecureStore from 'expo-secure-store';
 
-const api = axios.create({ baseURL: 'http://192.168.1.139:3000' });
+const api = axios.create({ baseURL: 'https://4b56-202-36-244-215.ngrok-free.app' });
 
 api.interceptors.request.use(async (config) => {
   const token = await SecureStore.getItemAsync('USER_TOKEN');
