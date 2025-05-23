9e8e50947069878a2e53626718351562f83e15da
andyx <andyxu024@gmail.com>
2025-05-24 00:23:50 +1200

    fixing routing

--- app/app/(auth)/_layout.tsx
+++ app/app/(auth)/_layout.tsx
@@ -24,7 +24,7 @@ function LayoutInner() {
 
           if (route.name === 'Modules') {
             return <MaterialIcons name="menu-book" size={size} color={iconColor} />;
-          } else if (route.name === 'Forum') {
+          } else if (route.name === 'Support') {
             return <MaterialIcons name="attribution" size={size} color={iconColor} />;
           } else if (route.name === 'Profile') {
             return (
@@ -58,7 +58,7 @@ function LayoutInner() {
       })}
     >
       <Tabs.Screen name="Modules" />
-      <Tabs.Screen name="Forum" />
+      <Tabs.Screen name="Support" />
       <Tabs.Screen name="Profile" />
     </Tabs>
   );
