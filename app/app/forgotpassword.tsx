31c19872cadede0cb34f0097590eac143ad5e37f
andyx <andyxu024@gmail.com>
2025-05-29 22:18:52 +1200

    refactored page

--- app/app/forgotpassword.tsx
+++ app/app/forgotpassword.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useContext } from 'react';
 import { Alert, ScrollView } from 'react-native';
 import { ThemeContext } from '@/contexts/ThemeContext';
-import { View, StyleSheet, Image, Keyboard, TouchableWithoutFeedback } from 'react-native';
+import { View, StyleSheet, Image } from 'react-native';
 
 import StyledText from '@/components/StyledText';
 import TextInputBox from '@/components/TextInputBox';
@@ -13,8 +13,7 @@ export default function ContactFormScreen() {
   const [email, setEmail] = useState('');
   const [contact, setContact] = useState('');
   const { theme, isDarkMode } = useContext(ThemeContext);
-  //link to cms/let the form be sent to Programme Consultant’s queue?
-  //do we need to add a text input box for the user to write their problem?
+
   const GETFORM_ENDPOINT = 'https://getform.io/f/bpjpjpvb';
 
   const handleSubmit = async () => {
@@ -67,7 +66,7 @@ export default function ContactFormScreen() {
 
     <View style={styles.appLogo}><Image source={logoSource} style={styles.logoImage}/></View>
     <StyledText type="label" style={styles.customTitle}>Forgot Password?</StyledText>
-    <StyledText type="label" style={[theme.text, styles.customMessage]}>Enter your account information below. Our team will follow up shortly to help you reset your password.</StyledText>
+    <StyledText type="label" style={[{ color: theme.text }, styles.customMessage]}>Enter your account information below. Our team will follow up shortly to help you reset your password.</StyledText>
 
       <TextInputBox
         placeholder="First Name"
@@ -122,8 +121,8 @@ const styles = StyleSheet.create({
     width: 200,
     height: 240,
     borderRadius: 10,
-    marginBottom: 40,
-    marginTop: 40,
+    marginBottom: 20,
+    marginTop: 7,
     justifyContent: 'center',
     alignItems: 'center',
     alignSelf: 'center',
