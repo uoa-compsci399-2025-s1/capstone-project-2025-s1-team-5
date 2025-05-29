af027b751df89cb0c093cb84943ca551c6a2cc88
charoncs <lhua218@aucklanduni.ac.nz>
2025-05-30 02:13:23 +1200

    deleting packagelock, yarnlock

--- app/app/lib/api.ts
+++ app/app/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import * as SecureStore from 'expo-secure-store';
 
-const api = axios.create({ baseURL: 'http://172.23.43.63:3000/api' });
+const api = axios.create({ baseURL: 'http://192.168.50.203:3000/api' });
 
 api.interceptors.request.use(async (config) => {
   const token = await SecureStore.getItemAsync('USER_TOKEN');
